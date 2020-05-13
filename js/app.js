38932762fd1a58f9854608a0a6e3704f8f5b450d
Бернова Анна <bast33@yandex.ru>
2020-05-13 15:45:00 +0300

    add fetching data from covid-monitor

--- js/app.js
+++ js/app.js
@@ -28,12 +28,19 @@ let app_data = [],
 /* ---------------------------------------------- */
 /*                API URL AND KEY                 */
 /* ---------------------------------------------- */
-/*
-fetch(`https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_days_by_country.php?country=country`, {
+function fetchData(user_country){
+fetch(`https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_days_by_country.php?country=${user_country}`, {
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "covid19-monitor-pro.p.rapidapi.com",
 			"x-rapidapi-key": "7e269ec140msh8a5df9cfc21b4b4p1c1e3ejsn9aba26afc6e0"
 		}
 	})
-*/
+	.then( response =>{
+		return response.json();
+	})
+	.then( data =>{
+		
+	})
+}
+fetchData(user_country);
