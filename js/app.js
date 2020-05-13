1af0e6408090a98748e5dcea69174ad280112d6b
Бернова Анна <bast33@yandex.ru>
2020-05-13 15:17:38 +0300

    add geoplagin und country code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
-/* ---------------------------------------------- */
-/*            CODE EXPLAINED TUTORIALS            */
-/*         www.youtube.com/CodeExplained          */
-/* ---------------------------------------------- */
+
+const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const newrecovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
+
+let app_data = [],
+	cases_list = [],
+	recovered_list = [],
+	deaths_list = [],
+	dates = [];
+
+	let country_code = geoplugin_countryCode();
+	let user_country;
+	country_list.forEach( country =>{
+		if(country.code == country_code){
+			user_country = country.name;
+		}
+	});
+	
+
 
 /* ---------------------------------------------- */
 /*                API URL AND KEY                 */
